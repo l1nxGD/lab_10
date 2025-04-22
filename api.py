a4a9d9d74b4ef7230f1197a38b54251063e69c64
l1nxGD <useer095698@gmail.com>
2025-04-22 21:27:56 +0300

    Update api.py

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ class Api:
 
       def __init__(self, cityName):
             self.cityName = cityName
-            self.apiKey = "5c0e6a096ab232d92dc3890fe8a59a3e"
+            self.apiKey = ""
 
       def getData(self):
             r = requests.post(f"https://api.openweathermap.org/data/2.5/weather?q={self.cityName}&appid={self.apiKey}")
